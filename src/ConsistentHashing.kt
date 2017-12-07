7d2d241dd7542b3fa15d66410b175b3c6ad487b5
june.kim <june.kim@kakaocorp.com>
2017-12-07 11:57:09 +0900

    add not commit files

---
+++ src/ConsistentHashing.kt
@@ -0,0 +1,18 @@
+val map: HashMap<String, Int> = hashMapOf(
+        "wwl1401test" to 1,
+        "eunmin" to 2,
+        "seed" to 3,
+        "joony" to 4,
+        "hogle" to 5)
+
+fun main(args: Array<String>) {
+    val s: Any = ConsistentHash()
+
+    s.addNode(node = "A")
+
+    println(consistentHashing("wwl1401test"))
+}
+
+fun consistentHashing(key: Any): Unit {
+    map.get(key)
+}
